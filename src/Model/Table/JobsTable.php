9a45d6e324cbb525c7f90ed5a3c4dbd5c48590dd
oriza <ahmadoriza@gmail.com>
2020-05-23 14:40:06 +0700

    Job table model

---
+++ src/Model/Table/JobsTable.php
@@ -0,0 +1,116 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Model\Table;
+
+use Cake\ORM\Query;
+use Cake\ORM\RulesChecker;
+use Cake\ORM\Table;
+use Cake\Validation\Validator;
+use Cake\Event\EventInterface;
+
+/**
+ * Jobs Model
+ *
+ * @property \App\Model\Table\CompaniesTable&\Cake\ORM\Association\BelongsTo $Companies
+ *
+ * @method \App\Model\Entity\Job newEmptyEntity()
+ * @method \App\Model\Entity\Job newEntity(array $data, array $options = [])
+ * @method \App\Model\Entity\Job[] newEntities(array $data, array $options = [])
+ * @method \App\Model\Entity\Job get($primaryKey, $options = [])
+ * @method \App\Model\Entity\Job findOrCreate($search, ?callable $callback = null, $options = [])
+ * @method \App\Model\Entity\Job patchEntity(\Cake\Datasource\EntityInterface $entity, array $data, array $options = [])
+ * @method \App\Model\Entity\Job[] patchEntities(iterable $entities, array $data, array $options = [])
+ * @method \App\Model\Entity\Job|false save(\Cake\Datasource\EntityInterface $entity, $options = [])
+ * @method \App\Model\Entity\Job saveOrFail(\Cake\Datasource\EntityInterface $entity, $options = [])
+ * @method \App\Model\Entity\Job[]|\Cake\Datasource\ResultSetInterface|false saveMany(iterable $entities, $options = [])
+ * @method \App\Model\Entity\Job[]|\Cake\Datasource\ResultSetInterface saveManyOrFail(iterable $entities, $options = [])
+ * @method \App\Model\Entity\Job[]|\Cake\Datasource\ResultSetInterface|false deleteMany(iterable $entities, $options = [])
+ * @method \App\Model\Entity\Job[]|\Cake\Datasource\ResultSetInterface deleteManyOrFail(iterable $entities, $options = [])
+ */
+class JobsTable extends Table
+{
+    /**
+     * Initialize method
+     *
+     * @param array $config The configuration for the Table.
+     * @return void
+     */
+    public function initialize(array $config): void
+    {
+        parent::initialize($config);
+
+        $this->setTable('jobs');
+        $this->setDisplayField('title');
+        $this->setPrimaryKey('id');
+
+        $this->belongsTo('Companies', [
+            'foreignKey' => 'company_id',
+            'joinType' => 'INNER',
+        ]);
+    }
+
+    /**
+     * Default validation rules.
+     *
+     * @param \Cake\Validation\Validator $validator Validator instance.
+     * @return \Cake\Validation\Validator
+     */
+    public function validationDefault(Validator $validator): Validator
+    {
+        $validator
+            ->integer('id')
+            ->allowEmptyString('id', null, 'create');
+
+        $validator
+            ->scalar('title')
+            ->maxLength('title', 100)
+            ->requirePresence('title', 'create')
+            ->notEmptyString('title');
+
+        $validator
+            ->scalar('description')
+            ->requirePresence('description', 'create')
+            ->notEmptyString('description');
+
+        $validator
+            ->integer('posted_by')
+            ->requirePresence('posted_by', 'create')
+            ->notEmptyString('posted_by');
+
+        $validator
+            ->dateTime('created_at')
+            ->requirePresence('created_at', 'create')
+            ->notEmptyDateTime('created_at');
+
+        $validator
+            ->dateTime('updated_at')
+            ->requirePresence('updated_at', 'create')
+            ->notEmptyDateTime('updated_at');
+
+        return $validator;
+    }
+
+    /**
+     * Returns a rules checker object that will be used for validating
+     * application integrity.
+     *
+     * @param \Cake\ORM\RulesChecker $rules The rules object to be modified.
+     * @return \Cake\ORM\RulesChecker
+     */
+    public function buildRules(RulesChecker $rules): RulesChecker
+    {
+        $rules->add($rules->existsIn(['company_id'], 'Companies'));
+
+        return $rules;
+    }
+
+    /**
+     * Before save.
+     */
+    public function beforeSave(EventInterface $event, $entity)
+    {
+        $entity->created_at = date('Y-m-d H:i:s');
+        $entity->updated_at = date('Y-m-d H:i:s');
+    }
+}
