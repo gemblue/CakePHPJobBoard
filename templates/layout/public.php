80b00ed69572cd00b4c2fe6605ebb2d19a376161
oriza <ahmadoriza@gmail.com>
2020-05-26 23:45:34 +0700

    Setup new layout, called public

---
+++ templates/layout/public.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <?= $this->Html->charset() ?>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>
+        <?= $title ?>
+    </title>
+    <?= $this->Html->meta('icon') ?>
+
+    <link href="https://fonts.googleapis.com/css?family=Raleway:400,700" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/normalize.css@8.0.1/normalize.css">
+    
+    <?= $this->Html->css('milligram.min.css') ?>
+    <?= $this->Html->css('cake.css') ?>
+    <?= $this->Html->css('home.css') ?>
+    
+    <?= $this->fetch('meta') ?>
+    <?= $this->fetch('css') ?>
+    <?= $this->fetch('script') ?>
+</head>
+<body>
+    <header>
+        <h2>JobsDB Like</h2>
+    </header>
+    
+    <div class="container">
+        <?= $this->Flash->render() ?>
+        <?= $this->fetch('content') ?>
+    </div>
+
+    <footer>
+        All Right Reserved &copy; 2020. Written in CakePHP v4 by <a href="https://github.com/gemblue" target="_blank">Gemblue</a>
+    </footer>
+</body>
+</html>
