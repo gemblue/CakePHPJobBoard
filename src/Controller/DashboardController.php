d243a456c811848be5585eb2089537be09b9e251
oriza <ahmadoriza@gmail.com>
2020-05-26 22:29:08 +0700

    Create dashboard controller

---
+++ src/Controller/DashboardController.php
@@ -0,0 +1,20 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Controller;
+
+/**
+ * Dashboard Controller
+ */
+class DashboardController extends AppController
+{
+    /**
+     * Index method
+     *
+     * @return \Cake\Http\Response|null|void Renders view
+     */
+    public function index()
+    {
+        
+    }
+}
