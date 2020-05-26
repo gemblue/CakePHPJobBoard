ea7fa30efe2ecb994611739e17f1f28e67541050
oriza <ahmadoriza@gmail.com>
2020-05-26 22:29:58 +0700

    Tweak layout base

--- templates/layout/default.php
+++ templates/layout/default.php
@@ -29,31 +29,39 @@ $cakeDescription = 'CakePHP: the rapid development php framework';
 
     <link href="https://fonts.googleapis.com/css?family=Raleway:400,700" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/normalize.css@8.0.1/normalize.css">
+    <link rel="stylesheet" href="https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css">
 
     <?= $this->Html->css('milligram.min.css') ?>
     <?= $this->Html->css('cake.css') ?>
-
+    <?= $this->Html->css('admin.css') ?>
+    
     <?= $this->fetch('meta') ?>
     <?= $this->fetch('css') ?>
     <?= $this->fetch('script') ?>
 </head>
 <body>
-    <nav class="top-nav">
-        <div class="top-nav-title">
-            <a href="/"><span>Cake</span>PHP</a>
-        </div>
-        <div class="top-nav-links">
-            <a target="_blank" href="https://book.cakephp.org/4/">Documentation</a>
-            <a target="_blank" href="https://api.cakephp.org/4/">API</a>
-        </div>
-    </nav>
-    <main class="main">
-        <div class="container">
+
+<div class="row">
+    <div class="column column">
+        <nav>
+            <h3>JobsDB Like</h3>
+            <a href="/home" target="_blank"><i class="las la-external-link-alt"></i> Open</a>
+            
+            <ul>
+                <li><a href="/dashboard">Dashboard</a></li>
+                <li><a href="/users">Users</a></li>
+                <li><a href="/jobs">Jobs</a></li>
+                <li><a href="/companies">Companies</a></li>
+            </ul>
+        </nav>
+    </div>
+    <div class="column column-80">
+        <main>
             <?= $this->Flash->render() ?>
             <?= $this->fetch('content') ?>
-        </div>
-    </main>
-    <footer>
-    </footer>
+        </main>
+    </div>
+</div>
+
 </body>
 </html>
