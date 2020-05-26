0e5424d0e52cf917eb354917d7973dd73bcf453d
oriza <ahmadoriza@gmail.com>
2020-05-26 22:29:38 +0700

    Dashboard index

---
+++ templates/Dashboard/index.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @var \App\View\AppView $this
+ * @var \App\Model\Entity\User[]|\Cake\Collection\CollectionInterface $users
+ */
+?>
+<div class="users index content">
+    <h3>Welcome</h3>
+    <p>Selamat datang di platform JobsDB like. Platform pencari kerja terkini.</p>
+</div>
