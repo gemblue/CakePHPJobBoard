9cd24e16774c4f20db5f76b120a90dde02ab3dbc
oriza <ahmadoriza@gmail.com>
2020-05-26 16:46:23 +0700

    User view detail template

---
+++ templates/Users/view.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * @var \App\View\AppView $this
+ * @var \App\Model\Entity\User $user
+ */
+?>
+<div class="row">
+    <aside class="column">
+        <div class="side-nav">
+            <h4 class="heading"><?= __('Actions') ?></h4>
+            <?= $this->Html->link(__('Edit User'), ['action' => 'edit', $user->id], ['class' => 'side-nav-item']) ?>
+            <?= $this->Form->postLink(__('Delete User'), ['action' => 'delete', $user->id], ['confirm' => __('Are you sure you want to delete # {0}?', $user->id), 'class' => 'side-nav-item']) ?>
+            <?= $this->Html->link(__('List Users'), ['action' => 'index'], ['class' => 'side-nav-item']) ?>
+            <?= $this->Html->link(__('New User'), ['action' => 'add'], ['class' => 'side-nav-item']) ?>
+        </div>
+    </aside>
+    <div class="column-responsive column-80">
+        <div class="users view content">
+            <h3><?= h($user->name) ?></h3>
+            <table>
+                <tr>
+                    <th><?= __('Name') ?></th>
+                    <td><?= h($user->name) ?></td>
+                </tr>
+                <tr>
+                    <th><?= __('Email') ?></th>
+                    <td><?= h($user->email) ?></td>
+                </tr>
+                <tr>
+                    <th><?= __('Username') ?></th>
+                    <td><?= h($user->username) ?></td>
+                </tr>
+                <tr>
+                    <th><?= __('Id') ?></th>
+                    <td><?= $this->Number->format($user->id) ?></td>
+                </tr>
+                <tr>
+                    <th><?= __('Created At') ?></th>
+                    <td><?= h($user->created_at) ?></td>
+                </tr>
+                <tr>
+                    <th><?= __('Updated At') ?></th>
+                    <td><?= h($user->updated_at) ?></td>
+                </tr>
+            </table>
+            <div class="text">
+                <strong><?= __('Biography') ?></strong>
+                <blockquote>
+                    <?= $this->Text->autoParagraph(h($user->biography)); ?>
+                </blockquote>
+            </div>
+        </div>
+    </div>
+</div>
