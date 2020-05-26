ec7873e666eece9a1ba178d95f0cf8d5d28fde40
oriza <ahmadoriza@gmail.com>
2020-05-26 16:24:08 +0700

    Remove old users management

--- templates/User/add.php
+++
@@ -1,7 +0,0 @@
-<h3><?php echo h($title) ?></h3>
-
-<form action="http://localhost/lab/CakePHP/user/insert" method="POST">
-    <input type="text" name="name" placeholder="Name"/>
-    <input type="text" name="email" placeholder="Email"/>
-    <button type="submit" class="button button-outline">Save</button>
-</form>
--- templates/User/search.php
+++
@@ -1,9 +0,0 @@
-<form action="http://localhost/lab/CakePHP/user/insert" method="POST">
-    <input type="text" name="email" placeholder="Kata Kunci .."/>
-    <button type="submit">Cari</button>
-</form>
-
-<table>
-<tr><th>Nama</th><th>Email</th></tr>
-<tr><td>Oriza</td><td>oriza@gmail.com</td></tr>
-</table>
