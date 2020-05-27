34d5b4a5f4b0fcdf7e34a040a5f79221b7c6d135
oriza <ahmadoriza@gmail.com>
2020-05-27 19:52:16 +0700

    Show company image, homepage

--- templates/Home/index.php
+++ templates/Home/index.php
@@ -7,16 +7,18 @@
                 <div class="box">
                     <h3><?= h($this->Text::truncate($job->title, 30, ['html' => true])) ?></h3>
                     
+                    <img src="<?php echo ($job->company->image) ? $job->company->image : 'https://fintechng.org/portal/assets/img/logo-default.svg';?>" style="width:50px;" alt="" />
+                    
                     <div class="company">
                         <?= $job->has('company') ? $this->Html->link($job->company->name, ['controller' => 'Companies', 'action' => 'view', $job->company->id]) : '' ?>
                     </div>
-                    
+                     
                     <p><?= h($this->Text::truncate($job->description, 100, ['html' => true]))?></p>
                     
                     <div style="margin-top:30px;">
                         <small>Diposting pada tanggal <?= h($job->created_at) ?></small>
                     </div>
-
+                
                     <div style="margin-top:20px;">
                         <a href="/detail/<?= $job->id ?>" class="button button-outline">Lihat</a>
                     </div>
